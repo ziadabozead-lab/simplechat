b1897636fee37a2b64a437da3de9753c41dfdeb4
ziadabozead-lab <ziadabozead31@gmail.com>
2026-07-29 22:44:08 +0300

    Update settings.py

--- simplechat/simplechat/settings.py
+++ simplechat/simplechat/settings.py
@@ -117,6 +117,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'timeout': 20,
+            'init_command': "PRAGMA journal_mode=WAL;",
+        },
     }
 }
 
